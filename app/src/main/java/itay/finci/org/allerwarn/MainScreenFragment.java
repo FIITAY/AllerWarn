35359216c7def4ffaff1455ffcdb4023459fb20b
Itay Finci <Itay@Finci.org>
2017-01-31 21:33:54 +0200

    Remove User
    -added option to delete user on swiping each direction.
    
    resolved #5
    Signed-off-by: Itay Finci <Itay@Finci.org>

--- app/src/main/java/itay/finci/org/allerwarn/MainScreenFragment.java
+++ app/src/main/java/itay/finci/org/allerwarn/MainScreenFragment.java
@@ -1,12 +1,15 @@
 package itay.finci.org.allerwarn;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +18,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainScreenFragment extends Fragment {
+import static android.R.id.list;
 
+public class MainScreenFragment extends Fragment {
+    ContactAdapter ca;
+    List<ContactInfo> al;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main_screen, container, false);
@@ -26,12 +32,51 @@ public class MainScreenFragment extends Fragment {
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recList.setLayoutManager(llm);
-
-        ContactAdapter ca = new ContactAdapter(createList(UserList.getInstance().size()));
+        al =createList(UserList.getInstance().size());
+        ca= new ContactAdapter(al);
         recList.setAdapter(ca);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recList); //set swipe to recylcerview
 
         return v;
     }
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+        @Override
+        public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction) {
+            final int position = viewHolder.getAdapterPosition(); //get position which is swipe
+
+            if (direction == ItemTouchHelper.LEFT || direction == ItemTouchHelper.RIGHT ) {    //if swipe left
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext()); //alert for confirm to delete
+                builder.setMessage("Are you sure to delete?");    //set message
+
+                builder.setPositiveButton("REMOVE", new DialogInterface.OnClickListener() { //when click on DELETE
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ca.notifyItemRemoved(position);    //item removed from recylcerview
+                        //list.remove(position);  //then remove item
+                        UserList.getInstance().remove(position);
+                        al.remove(position);
+                        return;
+                    }
+                }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {  //not removing items if cancel is done
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ca.notifyItemRemoved(position + 1);    //notifies the RecyclerView Adapter that data in adapter has been removed at a particular position.
+                        ca.notifyItemRangeChanged(position, ca.getItemCount());   //notifies the RecyclerView Adapter that positions of element in adapter has been changed from position(removed element index to end of list), please update it.
+                        return;
+                    }
+                }).show();  //show alert dialog
+            }
+        }
+    };
+
 
     private List<ContactInfo> createList(int size) {
 
